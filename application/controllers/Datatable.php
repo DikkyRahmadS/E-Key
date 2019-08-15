0cb50c5cfc559c82894cd22fb8d97633a0e179d3
Rahmat Lutvi Furkon <lutvi1500@gmail.com>
2019-08-15 11:53:38 +0700

    perbaikan waktu

--- application/controllers/Datatable.php
+++ application/controllers/Datatable.php
@@ -5,7 +5,8 @@ class Datatable extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('mdata');
+		$this->load->model('mdata');
+        date_default_timezone_set('Asia/Jakarta');
     }
 
     public function index()
